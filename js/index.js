12ae52785a96fef72d1fffe4fcdb69d12796777b
liutao3253 <1937373367@qq.com>
2018-03-28 10:04:24 +0800

    周边游订单详情

--- js/index.js
+++ js/index.js
@@ -1,44 +1,26 @@
 $(function(){
-//	 var tab = {
-//      tabChange: function(id){
-//        //切换到指定Tab项
-//        element.tabChange('xbs_tab', id); //切换到：用户管理
-//      }
-//    };
-    	$('.left-nav #nav li').click(function (event) {
-
+	$('.sub-menu').slideUp();
+    $('.left-nav #nav li').click(function (event) {
         if($(this).children('.sub-menu').length){
             if($(this).hasClass('open')){
                 $(this).removeClass('open');
-                $(this).find('.nav_right').html('&#xe6a7;');
+                $(this).find('.nav_right').removeClass('rotate');
                 $(this).children('.sub-menu').stop().slideUp();
                 $(this).siblings().children('.sub-menu').slideUp();
             }else{
                 $(this).addClass('open');
-                $(this).children('a').find('.nav_right').html('&#xe6a6;');
+                $(this).children('a').find('.nav_right').addClass('rotate');           
                 $(this).children('.sub-menu').stop().slideDown();
                 $(this).siblings().children('.sub-menu').stop().slideUp();
-                $(this).siblings().find('.nav_right').html('&#xe6a7;');
+                $(this).siblings().find('.nav_right').removeClass('rotate');
                 $(this).siblings().removeClass('open');
             }
         }else{
-
             var url = $(this).children('a').attr('_href');console.log($('.x-iframe').length)
-            var title = $(this).find('cite').html();
-            var index  = $('.left-nav #nav li').index($(this));
-
-//          for (var i = 0; i <$('.x-iframe').length; i++) {
-//              if($('.x-iframe').eq(i).attr('tab-id')==index+1){
-//                  tab.tabChange(index+1);
-					$('.x-iframe').attr('src',url)
-//                  event.stopPropagation();
-//                  return;
-//              }
-//          };
+//          var title = $(this).find('cite').html();
+//          var index  = $('.left-nav #nav li').index($(this));
+			$('.x-iframe').attr('src',url);
         }
-        
-        event.stopPropagation();
-         
     });
     
     //  退出
@@ -46,16 +28,16 @@ $(function(){
 		window.location.href='login.html'
 	});
 //	左侧栏回收
-	 $('.left_open i').click(function(event) {
+	 $('.left_open img').click(function(event) {
         if($('.left-nav').css('left')=='0px'){
-            $('.left-nav').animate({left: '-221px'}, 100);
+            $('.left-nav').animate({left: '-210px'}, 100);
             $('.page-content').animate({left: '0px'}, 100);
             $('.left_open').animate({left: '3px'}, 100);
             $('.page-content-bg').hide();
         }else{
             $('.left-nav').animate({left: '0px'}, 100);
             $('.page-content').animate({left: '200px'}, 100);
-            $('.left_open').animate({left: '205px'}, 100);
+            $('.left_open').animate({left: '190px'}, 100);
             if($(window).width()<768){
                 $('.page-content-bg').show();
             }
