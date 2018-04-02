323fd4085afa18d42ba281f85e93b3a601eb9cba
liutao3253 <1937373367@qq.com>
2018-04-02 20:14:43 +0800

    周边游新增商品价格日历数据修改

--- js/addtravelgoods.js
+++ js/addtravelgoods.js
@@ -64,33 +64,46 @@ $(function(){
 		lang : 'zh-cn',
 		firstDay:0,
 		selectHelper : true,
+		selectable:true,
 		eventLimit : true, // allow "more" link when too many events
 		monthNames :['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
 		dayNames: ['周日','周一','周二','周三','周四','周五','周六'],
-		
+		select: function(start,end){
+			var title = prompt('Event Title:');
+			var eventData;
+			if(title){
+				eventData={
+					title:title,
+					start:start,
+					end:end
+				};
+				$('#calendar').fullCalendar('renderEventSource', eventData,true);
+			}
+			$('#calendar').fullCalendar('unselect');
+		},
 		events: function(start, end, timezone, callback)
 		{
 		  	callback();
 		},
-		dayClick: function(date, allDay, jsEvent, view) {
- 			console.log(date, allDay, jsEvent, view);
-	    },
-//		eventClick : function(event, jsEvent, view) {
-//			
-//		 
-//		},
+		eventClick : function(event, jsEvent, view) {
+			
+		 
+		},
 		eventRender: function (event, element) {
 			var inputs = '';
-			inputs +='销售价<input type="" name="" id="salesPrice" value="" />';
-			inputs +='结算价<input type="" name="" id="setPrice" value="" />';
-			inputs +='库存<input type="" name="" id="stock" value="" />';
+			inputs +='销售价<input type="" name="" id="salesPrice" value="" onChange="getValue(this)"/>';
+			inputs +='结算价<input type="" name="" id="setPrice" value="" onChange="getValue(this)" />';
+			inputs +='库存<input type="" name="" id="stock" value="" onChange="getValue(this)"/>';
             element.html(inputs);
+            element.attr('index',event.start._i);
             element.children('input#salesPrice').val(event.salesPrice);
             element.children('input#setPrice').val(event.setPrice);
             element.children('input#stock').val(event.stock);
+            
 		},
 
 	});
+	
 	
 //	批量插入
 	$("#btnBatch").click(function(){
@@ -117,7 +130,32 @@ $(function(){
 		
 	})
 	
-})
+});
+
+//动态修改数据
+function getValue(p){
+	var id = $(p).attr('id');
+	var index = $(p).parent().attr('index');
+	var salesPrice,setPrice,stock;
+	
+	if(id=='salesPrice'){
+		salesPrice = $(p).val();
+	}else if(id=='setPrice'){
+		setPrice = $(p).val();
+	}else if(id=='stock'){
+		stock = $(p).val();
+	}
+	
+	$.each(eventArr,function(i,o){
+		if(o.start==index){
+			if(salesPrice){o.salesPrice=salesPrice;}
+			if(setPrice){o.setPrice=setPrice;}
+			if(stock){o.stock=stock;}
+			
+		}
+	})
+	console.log(salesPrice,setPrice,stock,index,eventArr);
+}
 //	按人/按份选择  
 function showImage(val){
 	if(val=='1'){
@@ -174,6 +212,7 @@ function copyText(saledatestart,saledateend,salesPrice,setPrice,stock,weekArr){
 * @param sDate2 结束日期 如：2016-11-02
 * @returns [Array] 返回相差天数
 */
+var eventArr=[];
 function getDatediff(sDate1,sDate2,weekArr){
 	
 	var start = Date.parse(new Date(sDate1));
@@ -181,7 +220,7 @@ function getDatediff(sDate1,sDate2,weekArr){
 //	相差天数
 	var nDays = Math.abs(parseInt((end - start)/1000/3600/24));
 	
-	var eventArr=[];
+	
 	var date = new Date(sDate1);
 	var enddate;
 	var startweek = date.getDay();
